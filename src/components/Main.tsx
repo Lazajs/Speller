9d67da68a2ee5dfa5db711c03e7decf1baa06c32
Lázaro Sánchez <lazarosanchezjs@gmail.com>
2023-04-21 01:24:20 -0300

    fix: sizes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,9 +7,9 @@ export default function Main () {
   const [showModal, setShowModal] = useState(false)
   return (
     <>
+    <h1 className="text-center mt-5 text-5xl font-bold">Speller.</h1>
     {showModal ? <Modal toggle={setShowModal} /> : (
       <section className="w-full mt-8 max-w-[600px] m-auto">
-        <h1 className="text-center text-5xl font-bold">Speller.</h1>
         <span className="flex justify-between items-center m-2">
           <h2 className="text-xl lg:text-2xl m-2 opacity-8">The AI will fix...</h2>
           <h2 onClick={()=> setShowModal(true)} className="inline-block cursor-pointer underline text-xl lg:text-2xl m-2 opacity-8">What is this...?</h2>
